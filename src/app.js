b60025fde0266ff82d7e213a1229b7b2dd4a34dc
unknown <vansh.100.2020@doonschool.com>
2020-05-20 18:21:22 +0530

    Bug fix

--- src/app.js
+++ src/app.js
@@ -10,25 +10,30 @@ var client = new Twitter({
     access_token_secret: 'AaBY8W6g985toPCl3OZ4rzG3XmursK2ROZcsjxNNfCw3X'
 });
 
-
+var i=1
 
 
 
 const tweet=()=>{
+    console.log('Vansh')
     quote().then((response)=>{
         quote_day=response.contents.quotes[0].quote
         client.post('statuses/update', {status: '#QuoteOfTheDay \"'+quote_day+'\"'},  function(error, tweet, response) {
-            if(error) throw error;
-            console.log(tweet);  // Tweet body.
-            console.log(response);  // Raw response object.
-        });
+            if(error){
+                console.log('error')
+            }else{
+                console.log(tweet);  // Tweet body.
+                console.log(response);  // Raw response object.
+            }
+            
+        })
     }).catch((e)=>{
-        console.log(e)
+        console.log('error')
     })
     
 }
 
 tweet()
 
-setInterval(tweet, 86400000);
+setInterval(tweet, 10000);
 
